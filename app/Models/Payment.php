f166e8b0b5416628c564ee7ac40222175c2a32de
PedroLucca <pedrolucca27@gmail.com>
2026-03-19 21:34:03 -0300

    feat: criando outros models restantes

---
+++ app/Models/Payment.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class Payment extends Model
+{
+    use SoftDeletes;
+
+    protected $table = 'payment';
+
+    protected $fillable = [
+        'name',
+        'description',
+        'price',
+        'stock'
+    ];
+
+}
---
+++ app/Models/ShoppingCartProducts.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+
+class ShoppingCartProducts extends Model {
+    
+    use SoftDeletes;
+
+    protected $table = 'shopping_cart_products';
+
+    protected $fillable = [
+        'user_id',
+        'product_id',
+        'amount',
+        'status'
+    ];
+
+    public function user(){
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function product(){
+        return $this->belongsTo(Product::class, 'product_id');
+    }
+
+}
